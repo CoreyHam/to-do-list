4295235fb51264c8f0c7c35328f1aea24f61f3cd
Corey <corey.hampton@student.awesomeinc.org>
2022-03-22 17:39:02 +0000

    Setup Components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import ClearButton from './components/ClearButton';
+import Display from './components/Display';
+import FilterButtons from './components/FilterButtons';
+import Input from './components/Input';
+import Todos from './components/Todos';
 
-function App() {
+export default function App() {
   return (
     <div className="app">
+      <Display />
+      <Input />
+      <Todos />
+      <FilterButtons />
+      <ClearButton />
     </div>
   );
 }
-
-export default App;
